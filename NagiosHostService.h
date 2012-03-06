e3582df25c47a16b38e141894f318f981f44f38a
Jason Myerscough <jason@Jasons-MacBook-Air.local>
2012-03-06 22:25:24 +0100

    implemented the properties

--- NagiosHostService.h
+++ NagiosHostService.h
@@ -10,4 +10,57 @@
 
 @interface NagiosHostService : NSObject
 
+@property (nonatomic, strong) NSString *hostname;
+@property (nonatomic, strong) NSString *serviceDescription;
+@property (nonatomic) NSInteger modifiedAttributes;
+@property (nonatomic, strong) NSString *checkCommand;
+@property (nonatomic, strong) NSString *checkPeriod;
+@property (nonatomic, strong) NSString *notificationPeriod;
+@property (nonatomic) float checkInterval;
+@property (nonatomic) float retryInterval;
+@property (nonatomic, strong) NSString *eventHandler;
+@property (nonatomic) BOOL hasBeenChecked;
+@property (nonatomic) BOOL shouldBeScheduled;
+@property (nonatomic) float checkExecutionTime;
+@property (nonatomic) float checkLatency;
+@property (nonatomic) NSInteger checkType;
+@property (nonatomic) NSInteger currentState;
+@property (nonatomic) NSInteger lastHardState;
+@property (nonatomic) NSInteger lastEventId;
+@property (nonatomic) NSInteger currentEventId;
+@property (nonatomic) NSInteger currentProblemId;
+@property (nonatomic) NSInteger lastProblemId;
+@property (nonatomic) NSInteger currentAttempt;
+@property (nonatomic) NSInteger maxAttempts;
+@property (nonatomic) NSInteger stateType;
+@property (nonatomic) long lastStateChange;
+@property (nonatomic) long lastHardStateChange;
+@property (nonatomic) long lastTimeOk;
+@property (nonatomic) long lastTimeWarning;
+@property (nonatomic) long lastTimeUnknown;
+@property (nonatomic) long lastTimeCritical;
+@property (nonatomic, strong) NSString *pluginOutput;
+@property (nonatomic, strong) NSString *performanceData;
+@property (nonatomic) long lastCheck;
+@property (nonatomic) long nextCheck;
+@property (nonatomic) NSInteger checkOptions;
+@property (nonatomic) NSInteger currentNotificationNumber;
+@property (nonatomic) NSInteger currentNotificationId;
+@property (nonatomic) NSInteger lastNotification;
+@property (nonatomic) NSInteger nextNotification;
+@property (nonatomic) BOOL noMoreNotifications;
+@property (nonatomic) BOOL notificationsEnabled;
+@property (nonatomic) BOOL activeChecksEnabled;
+@property (nonatomic) BOOL passiveChecksEnabled;
+@property (nonatomic) BOOL eventHandlerEnabled;
+@property (nonatomic) BOOL problemHasBeenAcknowledged;
+@property (nonatomic) NSInteger acknowledgementType;
+@property (nonatomic) BOOL flapPredictionEnabled;
+@property (nonatomic) BOOL processPerformanceData;
+@property (nonatomic) BOOL obsessOverService;
+@property (nonatomic) long lastUpdate;
+@property (nonatomic) BOOL isFlapping;
+@property (nonatomic) float percentStateChange;
+@property (nonatomic) NSInteger scheduledDowntimeDepth;
+
 @end
--- NagiosHostService.m
+++ NagiosHostService.m
@@ -10,4 +10,58 @@
 
 @implementation NagiosHostService
 
+@synthesize hostname = _hostname;
+@synthesize serviceDescription = _serviceDescription;
+@synthesize modifiedAttributes = _modifiedAttributes;
+@synthesize checkCommand = _checkCommand;
+@synthesize checkPeriod = _checkPeriod;
+@synthesize notificationPeriod = _notificationPeriod;
+@synthesize checkInterval = _checkInterval;
+@synthesize retryInterval = _retryInterval;
+@synthesize eventHandler = _eventHandler;
+@synthesize hasBeenChecked = _hasBeenChecked;
+@synthesize shouldBeScheduled = _shouldBeScheduled;
+@synthesize checkExecutionTime = _checkExecutionTime;
+@synthesize checkLatency = _checkLatency;
+@synthesize checkType = _checkType;
+@synthesize currentState = _currentState;
+@synthesize lastHardState = _lastHardState;
+@synthesize lastEventId = _lastEventId;
+@synthesize currentEventId = _currentEventId;
+@synthesize currentProblemId = _currentProblemId;
+@synthesize lastProblemId = _lastProblemId;
+@synthesize currentAttempt = _currentAttempt;
+@synthesize maxAttempts = _maxAttempts;
+@synthesize stateType = _stateType;
+@synthesize lastStateChange = _lastStateChange;
+@synthesize lastHardStateChange = _lastHardStateChange;
+@synthesize lastTimeOk = _lastTimeOk;
+@synthesize lastTimeWarning = _lastTimeWarning;
+@synthesize lastTimeUnknown = _lastTimeUnknown;
+@synthesize lastTimeCritical = _lastTimeCritical;
+@synthesize pluginOutput = _pluginOutput;
+@synthesize performanceData = _performanceData;
+@synthesize lastCheck = _lastCheck;
+@synthesize nextCheck = _nextCheck;
+@synthesize checkOptions = _checkOptions;
+@synthesize currentNotificationNumber = _currentNotificationNumber;
+@synthesize currentNotificationId = _currentNotificationId;
+@synthesize lastNotification = _lastNotification;
+@synthesize nextNotification = _nextNotification;
+@synthesize noMoreNotifications = _noMoreNotifications;
+@synthesize notificationsEnabled = _notificationsEnabled;
+@synthesize activeChecksEnabled = _activeChecksEnabled;
+@synthesize passiveChecksEnabled = _passiveChecksEnabled;
+@synthesize eventHandlerEnabled = _eventHandlerEnabled;
+@synthesize problemHasBeenAcknowledged = _problemHasBeenAcknowledged;
+@synthesize acknowledgementType = _acknowledgementType;
+@synthesize flapPredictionEnabled = _flapPredictionEnabled;
+@synthesize processPerformanceData = _processPerformanceData;
+@synthesize obsessOverService = _obsessOverService;
+@synthesize lastUpdate = _lastUpdate;
+@synthesize isFlapping = _isFlapping;
+@synthesize percentStateChange = _percentStateChange;
+@synthesize scheduledDowntimeDepth = _scheduledDowntimeDepth;
+
+
 @end
