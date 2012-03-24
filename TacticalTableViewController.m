658afc1e5ef364df57b48a23991cef2dd2e58fce
Jason Myerscough <jason@Jasons-MacBook-Air.local>
2012-03-24 01:54:12 +0100

    capable of displaying basic tactical information

--- TacticalTableViewController.m
+++ TacticalTableViewController.m
@@ -17,10 +17,19 @@
 
 @end
 
-#define TACTICAL_SECTION_COUNT  3
-#define NETWORK_HEALTH_SECTION  0
-#define HOSTS_SECTION           1
-#define SERVICES_SECTION        2
+#define TACTICAL_SECTION_COUNT          2
+#define HOST_ROW_COUNT_IN_TACTICAL_VIEW 4
+#define NETWORK_HEALTH_SECTION          0
+#define HOSTS_SECTION                   1 
+#define SERVICES_SECTION                2 
+#define HOST_OK_INDEX                   0
+#define HOST_DOWN_INDEX                 1
+#define HOST_PENDING_INDEX              2
+#define HOST_UNREACHABLE_INDEX          3
+
+#define HEALTH_STATUS_HEADER            @"Network Health"
+#define HOST_SUMMARY_HEADER             @"Hosts"
+#define SERVICES_SUMMARY_HEADER         @"Services"
 
 
 @implementation TacticalTableViewController
@@ -46,7 +55,7 @@
     NagiosWebServiceReader *nagiosService = [[NagiosWebServiceReader alloc] initWithURL:
                                              [[NSURL alloc] initWithString:@"http://192.168.44.130/status/"]];
     self.hostCollection =  [nagiosService retrieveNagiosStatusAndBlock];
-    nagiosService = nil;
+    //nagiosService = nil;
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
@@ -63,24 +72,24 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    int serviceCount = 0;
-    NagiosHost *currentHost = nil;
+    //int serviceCount = 0;
+    //NagiosHost *currentHost = nil;
     
     switch (section)
     {
         case NETWORK_HEALTH_SECTION: return 2;
-        case HOSTS_SECTION: return [self.hostCollection count];
-        case SERVICES_SECTION:
-            for (int idx=0; idx < [self.hostCollection count]; idx++)
-            {
-                currentHost = [self.hostCollection objectAtIndex:idx];
-                if (currentHost)
-                {
-                    if (currentHost.services)
-                        serviceCount += [currentHost.services count];
-                }
-            }
-            return serviceCount;
+        case HOSTS_SECTION: return HOST_ROW_COUNT_IN_TACTICAL_VIEW;
+        //case SERVICES_SECTION:
+        //    for (int idx=0; idx < [self.hostCollection count]; idx++)
+        //    {
+        //        currentHost = [self.hostCollection objectAtIndex:idx];
+        //        if (currentHost)
+        //        {
+        //            if (currentHost.services)
+        //                serviceCount += [currentHost.services count];
+        //        }
+        //    }
+        //    return serviceCount;
     }
 
     return 0;
@@ -98,34 +107,92 @@
             if (!indexPath.row)
             {
                 int upHostCount = 0;
+                float status = 0;
                 
                 for (int idx=0; idx < [self.hostCollection count]; idx++)
                 {
                     NagiosHost *host = [self.hostCollection objectAtIndex:idx];
-                    if (host.currentState == NagiosServiceOk)
+                    if (host.currentState == 1)
                         ++upHostCount;
                 }
+                
+                if (self.hostCollection)
+                {
+                    int c = [self.hostCollection count];
+                    status = (c / upHostCount) * 100;
+                }
+                else {
+                    status = 0;
+                }
                 cell.textLabel.text = @"Hosts";
-                cell.detailTextLabel.text = [NSString stringWithFormat:@"%d%%", ([self.hostCollection count] / upHostCount) * 100];
+                cell.detailTextLabel.text = [NSString stringWithFormat:@"%.2f%\%", status];
                 return cell;
             }
             else
             {
                 cell.textLabel.text = @"Services";
-                cell.detailTextLabel.text = @"100%";
+                cell.detailTextLabel.text = @"100%";    // TODO implement service information
             }
             break;
         }
         case HOSTS_SECTION:
-            break;
-        case SERVICES_SECTION:
-            break;            
-        default:
-            break;
+        {
+            int okCount = 0, downCount = 0, pendingCount = 0, unreachableCount = 0;
+            
+            for (int idx = 0; idx < [self.hostCollection count]; idx++)
+            {
+                NagiosHost *host = [self.hostCollection objectAtIndex:idx];
+                
+                if (host.currentState == 1) ++okCount;
+            }
+            
+            switch (indexPath.row)
+            {
+                case HOST_OK_INDEX:
+                    cell.textLabel.text = @"UP";
+                    cell.detailTextLabel.text = [NSString stringWithFormat:@"%d", okCount];
+                    break;
+                case HOST_DOWN_INDEX:
+                    cell.textLabel.text = @"Down";
+                    cell.detailTextLabel.text = [NSString stringWithFormat:@"%d", downCount];
+                    break;
+                case HOST_PENDING_INDEX:
+                    cell.textLabel.text = @"Pending";
+                    cell.detailTextLabel.text = [NSString stringWithFormat:@"%d", pendingCount];
+                    break;
+                case HOST_UNREACHABLE_INDEX:
+                    cell.textLabel.text = @"Unreachable";
+                    cell.detailTextLabel.text = [NSString stringWithFormat:@"%d", unreachableCount];
+                    break;                    
+                default:
+                    break;
+            }
+            
+            
+            return cell;
+        }
+        //case SERVICES_SECTION:
+        //    break;            
+        //default:
+        //    break;
     }
     
     
     return cell;
+}
+
+// Sets the sections header
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    switch (section)
+    {
+        case NETWORK_HEALTH_SECTION:
+            return HEALTH_STATUS_HEADER;
+        case HOSTS_SECTION:
+            return HOST_SUMMARY_HEADER;
+        default:
+            return nil;
+    }
 }
 
 #pragma mark - UITableViewDelegate
