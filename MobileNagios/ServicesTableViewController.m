fc10b7e04c3b55c83aec3ec1a8b1bc02e85ac86a
Jason Myerscough <jason@Jasons-MacBook-Air.local>
2012-04-08 17:17:15 +0200

    added code to order the services

--- MobileNagios/ServicesTableViewController.m
+++ MobileNagios/ServicesTableViewController.m
@@ -9,21 +9,30 @@
 #import "ServicesTableViewController.h"
 #import "NagiosHostService.h"
 
+#define SERVICE_SECTION_COUNT          5
+
+#define SERVICE_OK_SECTION             0       
+#define SERVICE_DOWN_SECTION           1
+#define SERVICE_CRITICAL_SECTION       2
+#define SERVICE_PENDING_SECTION        3
+#define SERVICE_UNKNOWN_SECTION        4
+
+#define SERVICE_OK_HEADER             @"Ok"       
+#define SERVICE_DOWN_HEADER           @"Down"
+#define SERVICE_PENDING_HEADER        @"Pending"
+#define SERVICE_CRITICAL_HEADER       @"Critical"
+#define SERVICE_UNKNOWN_HEADER        @"Unknown"
+
 @interface ServicesTableViewController ()
-
-@property (nonatomic, strong) NSArray *serviceCollection;
-
 @property (nonatomic, strong) NSMutableArray *okServicesCollection;
 @property (nonatomic, strong) NSMutableArray *warningServicesCollection;
 @property (nonatomic, strong) NSMutableArray *criticalServicesCollection;
 @property (nonatomic, strong) NSMutableArray *pendingServicesCollection;
 @property (nonatomic, strong) NSMutableArray *unknownServicesCollection;
-
 @end
 
 @implementation ServicesTableViewController
 
-@synthesize serviceCollection = _serviceCollection;
 @synthesize okServicesCollection = _okServicesCollection;
 @synthesize warningServicesCollection = _warningServicesCollection;
 @synthesize criticalServicesCollection = _criticalServicesCollection;
@@ -32,28 +41,89 @@
 
 - (void)setHostCollection:(NSArray *)serviceCollection
 {
-    self.serviceCollection = serviceCollection;
+    if (!self.okServicesCollection) self.okServicesCollection = [[NSMutableArray alloc] init];
+    if (!self.warningServicesCollection) self.warningServicesCollection = [[NSMutableArray alloc] init];
+    if (!self.criticalServicesCollection) self.criticalServicesCollection = [[NSMutableArray alloc] init];
+    if (!self.pendingServicesCollection) self.pendingServicesCollection = [[NSMutableArray alloc] init];
+    if (!self.unknownServicesCollection) self.unknownServicesCollection = [[NSMutableArray alloc] init];
+    
+    // remove all elements from the status arrays
+    [self.okServicesCollection removeAllObjects];
+    [self.warningServicesCollection removeAllObjects];
+    [self.criticalServicesCollection removeAllObjects];
+    [self.pendingServicesCollection removeAllObjects];    
+    [self.unknownServicesCollection removeAllObjects];
+    
+    // add the services to the different status arrays
+    for (int idx = 0; idx < [serviceCollection count]; idx++)
+    {
+        NagiosHostService *service = [serviceCollection objectAtIndex:idx];
+        
+        switch (service.currentState)
+        {
+            case NagiosServiceOk: [self.okServicesCollection addObject:service]; break;
+            case NagiosServiceWarning: [self.warningServicesCollection addObject:service]; break;
+            case NagiosServiceCrticial: [self.criticalServicesCollection addObject:service]; break;
+            case NagiosServicePending: [self.pendingServicesCollection addObject:service]; break;
+            case NagiosServiceUnknown: [self.unknownServicesCollection addObject:service]; break;
+        }
+    }
 }
-
 
 #pragma mark - Table view data source
 
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return SERVICE_SECTION_COUNT;
+}
+
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return [self.serviceCollection count];
+    switch (section)
+    {
+        case SERVICE_OK_SECTION : return [self.okServicesCollection count];
+        case SERVICE_DOWN_SECTION: return [self.warningServicesCollection count];
+        case SERVICE_CRITICAL_SECTION: return [self.criticalServicesCollection count];
+        case SERVICE_PENDING_SECTION: return [self.pendingServicesCollection count];
+        case SERVICE_UNKNOWN_SECTION: return [self.unknownServicesCollection count];
+        default: return 0; // should never reach here
+    }
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     static NSString *CellIdentifier = @"Services Cell";
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-    NagiosHostService *service = [self.serviceCollection objectAtIndex:indexPath.row];
+    NagiosHostService *service = nil;
+    
+    switch (indexPath.section)
+    {
+        case SERVICE_OK_SECTION : service = [self.okServicesCollection objectAtIndex:indexPath.row]; break;
+        case SERVICE_DOWN_SECTION: service = [self.warningServicesCollection objectAtIndex:indexPath.row]; break;
+        case SERVICE_CRITICAL_SECTION: service = [self.criticalServicesCollection objectAtIndex:indexPath.row]; break;
+        case SERVICE_PENDING_SECTION: service = [self.pendingServicesCollection objectAtIndex:indexPath.row]; break;
+        case SERVICE_UNKNOWN_SECTION: service = [self.unknownServicesCollection objectAtIndex:indexPath.row]; break;
+        default: return 0; // should never reach here
+    }
     
     // Configure the cell...
     cell.textLabel.text = service.serviceDescription;
     cell.detailTextLabel.text = service.pluginOutput;
     
     return cell;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    switch (section)
+    {
+        case SERVICE_OK_SECTION : return SERVICE_OK_HEADER;
+        case SERVICE_DOWN_SECTION: return SERVICE_DOWN_HEADER;
+        case SERVICE_CRITICAL_SECTION: return SERVICE_CRITICAL_HEADER;
+        case SERVICE_PENDING_SECTION: return SERVICE_PENDING_HEADER;
+        case SERVICE_UNKNOWN_SECTION: return SERVICE_UNKNOWN_HEADER;
+        default: return @""; // should never reach here
+    }
 }
 
 #pragma mark - Table view delegate
